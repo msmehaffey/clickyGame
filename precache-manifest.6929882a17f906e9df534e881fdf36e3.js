d8817c91311a10e8d0e853a96e7772005bbeda3b
Shuler <msmehaf2@gmail.com>
2019-03-16 19:25:02 -0500

    Updates

---
+++ precache-manifest.6929882a17f906e9df534e881fdf36e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "e4decd366750d5c6b8f6",
+    "url": "/clickyGame/static/js/main.e034531f.chunk.js"
+  },
+  {
+    "revision": "a252e227d3244d802167",
+    "url": "/clickyGame/static/js/2.31a730b4.chunk.js"
+  },
+  {
+    "revision": "e4decd366750d5c6b8f6",
+    "url": "/clickyGame/static/css/main.60d29627.chunk.css"
+  },
+  {
+    "revision": "019239453288dd1f27ec6258f3c49af4",
+    "url": "/clickyGame/index.html"
+  }
+];
